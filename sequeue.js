b4e1c0bc89ca10f8290c1a5c401c9ce44ca8e5c3
André Kussmann <zoddy@zoddy.de>
2015-04-09 18:00:00 +0200

    make the code shorter (currently we don't need UUID's) and bump to new version

--- sequeue.js
+++ sequeue.js
@@ -3,7 +3,7 @@
 
 
 var sequeue = {
-  _queues: {},
+  _queues: [],
 
 
   /**
@@ -13,30 +13,16 @@ var sequeue = {
    * @return {string} the generated uuid for the queue
    */
   add: function(list) {
-    var queueId = this._createId(); // create UUID
-
     // create queue object
-    this._queues[queueId] = {
+    var queueId = this._queues.push({
       step: 0,
       list: list.concat(this._remove.bind(this, queueId))
-    };
+    }) - 1;
 
     // start queue
     process.nextTick(this._next.bind(this, queueId));
 
     return queueId;
-  },
-
-  /**
-   * creates a UUIDv4
-   * thanks to https://gist.github.com/jed/982883
-   *
-   * @return {string} the generated UUID
-   */
-  _createId: function(a) {
-    return a ?
-      (a ^ Math.random() * 16 >> a / 4).toString(16) :
-      ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, this._createId);
   },
 
   /**

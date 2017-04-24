09587c671cadcc8c1fbb3405475a0c84a93c8912
André Kussmann <zoddy@zoddy.de>
2017-04-24 18:03:02 +0200

    follow standard code

--- sequeue.js
+++ sequeue.js
@@ -1,26 +1,25 @@
+/* global define */
+
 'use strict';
-
-
 
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define([], factory);
+    define([], factory)
   } else if (typeof exports === 'object') {
-    module.exports = factory();
+    module.exports = factory()
   } else {
-    root.sequeue = factory();
+    root.sequeue = factory()
   }
 }(this, function () {
-
   /**
    * handles the queue
    *
    * @param {array<function>} queue list of functions
    */
-  var sequeue = function(queue) {
-    queue = [].concat(queue);
-    setTimeout(queue.shift().bind(null, sequeue.bind(null, queue)), 0);
-  };
+  var sequeue = function (queue) {
+    queue = [].concat(queue)
+    setTimeout(queue.shift().bind(null, sequeue.bind(null, queue)), 0)
+  }
 
-  return sequeue;
-}));
+  return sequeue
+}))
